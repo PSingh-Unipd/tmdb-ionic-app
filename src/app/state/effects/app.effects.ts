213ed40e01e548e1dec2005f3a3aae621a05ca93
parwinder.singh <parwinder.singh@redstor.com>
2023-10-31 15:49:32 +0000

    prettier

--- src/app/state/effects/app.effects.ts
+++ src/app/state/effects/app.effects.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { EMPTY } from 'rxjs';
-import { map, exhaustMap, catchError, tap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AppEffects {
-
-    loadMovies$ = createEffect(() => this.actions$.pipe(
+  loadMovies$ = createEffect(
+    () =>
+      this.actions$.pipe(
         ofType('[Movies Page] Load App'),
-        tap(() => console.log('effect')),
+        tap(() => console.log('effect'))
+      ),
+    { dispatch: false }
+  );
 
-    ), { dispatch: false });
-
-    constructor(
-        private actions$: Actions,
-    ) { }
+  constructor(private actions$: Actions) {}
 }
